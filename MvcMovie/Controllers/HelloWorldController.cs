cf709d1bf11c07b4229d27d0d52c6894bd0edb46
Christian Felipe Lopes Dias <Christian_Felipe9@hotmail.com>
2019-05-14 08:36:23 -0300

    Model Movies and Connection string added. EF 6.2 included in packages.

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -8,21 +8,17 @@ namespace MvcMovie.Controllers
 {
 	public class HelloWorldController : Controller
 	{
-		// 
-		// GET: /HelloWorld/ 
-
 		public ActionResult Index()
 		{
 			return View();
 		}
 
-		// 
-		// GET: /HelloWorld/Welcome/ 
-
-		public string Welcome(string name, int ID = 1)
-
+		public ActionResult Welcome(string name, int numtimes = 1)
 		{
-			return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+			ViewBag.Message = "Hello " + name;
+			ViewBag.NumTimes = numtimes;
+
+			return View();
 		}
 	}
 }
